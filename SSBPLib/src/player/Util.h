20c69e9affeb61a413819df3b704480b8b308971
kagematya <kagematya@gmail.com>
2017-01-15 04:47:06 +0900

    マクロ整理

--- SSBPLib/src/player/Util.h
+++ SSBPLib/src/player/Util.h
@@ -13,17 +13,11 @@ static double SSRadToDeg(double rad){
 static double SSDegToRad(double deg){
 	return deg * (SS_PI / 180.0);
 }
-//#define SSRadianToDegree(Radian) ((float)( Radian * __SS2PI__ )/ 360.0f )
-//#define SSDegreeToRadian(Degree) ((float)( Degree * 360.0f) / __SS2PI__)
 
 
-#define SS_SAFE_DELETE(p)            do { if(p) { delete (p); (p) = 0; } } while(0)
-#define SS_SAFE_DELETE_ARRAY(p)     do { if(p) { delete[] (p); (p) = 0; } } while(0)
-#define SS_SAFE_FREE(p)                do { if(p) { free(p); (p) = 0; } } while(0)
-#define SS_SAFE_RELEASE(p)            do { if(p) { (p)->release(); } } while(0)
-#define SS_SAFE_RELEASE_NULL(p)        do { if(p) { (p)->release(); (p) = 0; } } while(0)
-#define SS_SAFE_RETAIN(p)            do { if(p) { (p)->retain(); } } while(0)
-#define SS_BREAK_IF(cond)            if(cond) break
+#define SS_SAFE_DELETE(p)           { delete (p); (p) = nullptr; }
+#define SS_SAFE_DELETE_ARRAY(p)     { delete[] (p); (p) = nullptr; }
+
 
 #ifdef _DEBUG
 #define SSLOG(...)       do {} while (0)
