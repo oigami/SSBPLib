6079790fca84917ba60104cd552cbf809829f535
kagematya <kagematya@gmail.com>
2017-05-04 19:01:55 +0900

    テクスチャの事前読み込みを行うサンプルを追加

---
+++ DxLibSample/TexturePreload/main.cpp
@@ -0,0 +1,58 @@
+﻿#include "DxLib.h"
+#include "Application.h"
+
+static const int FPS = 60;
+
+//フレームレートの調整
+void framerateControll(){
+	static int previousTime;
+
+	int updateTime = GetNowCount() - previousTime;	//1フレームにかかった時間
+	int waitTime = (1000.0 / FPS) - updateTime;		//待つべき時間
+	if(waitTime > 0){
+		WaitTimer(waitTime);
+	}
+	previousTime = GetNowCount();
+}
+
+/**
+* メイン関数
+*/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+#endif
+
+
+	//DXライブラリの初期化
+	ChangeWindowMode(true);	//ウインドウモード
+	SetGraphMode(800, 600, GetColorBitDepth());
+	if(DxLib_Init() == -1){	// ＤＸライブラリ初期化処理
+		return -1;				// エラーが起きたら直ちに終了
+	}
+	SetDrawScreen(DX_SCREEN_BACK);
+
+
+	//アプリケーション初期化
+	Application application;
+	application.initialize();
+
+	//メインループ
+	while(ProcessMessage() == 0 && ClearDrawScreen() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0){	//esc押しで終了
+		application.update();	//ゲームの更新
+		application.draw();		//ゲームの描画
+
+		ScreenFlip();			//描画結果を画面に反映
+		framerateControll();	//FPS調整
+	}
+
+	//プレイヤー終了処理
+	application.finalize();
+
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
+	return 0;				// ソフトの終了 
+}
+
+
