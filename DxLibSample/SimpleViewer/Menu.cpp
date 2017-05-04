a9fb4163e629600ac4ebaf71d69ae65bacb0c72c
kagematya <kagematya@gmail.com>
2017-05-05 03:34:47 +0900

    ビューアーのサンプル追加中

---
+++ DxLibSample/SimpleViewer/Menu.cpp
@@ -0,0 +1,216 @@
+﻿#include "Menu.h"
+#include <sstream>
+#include <algorithm>
+#include "ss/SS5Player.h"
+using namespace std;
+using namespace ss;
+
+
+MenuRoot::MenuRoot(ss::SS5Player* p) : Base(p)
+	, m_activeIndex(0)
+{
+	m_childMenu.push_back(new AnimeChanger(p));
+	m_childMenu.push_back(new FrameChanger(p));
+	m_childMenu.push_back(new PositionChanger(p, PositionChanger::XYZ::X, 400));
+	m_childMenu.push_back(new PositionChanger(p, PositionChanger::XYZ::Y, 200));
+	m_childMenu.push_back(new RotationChanger(p, RotationChanger::XYZ::X));
+	m_childMenu.push_back(new RotationChanger(p, RotationChanger::XYZ::Y));
+	m_childMenu.push_back(new RotationChanger(p, RotationChanger::XYZ::Z));
+	m_childMenu.push_back(new ScaleChanger(p, ScaleChanger::XYZ::X));
+	m_childMenu.push_back(new ScaleChanger(p, ScaleChanger::XYZ::Y));
+	m_childMenu.push_back(new AlphaChanger(p));
+}
+
+MenuRoot::~MenuRoot()
+{
+	for(MenuInterface* child : m_childMenu){
+		delete child;
+	}
+	m_childMenu.clear();
+}
+
+void MenuRoot::action(int up, int down, int left, int right, int enter, int cancel)
+{
+	//メニュー項目の選択
+	if(down){
+		m_activeIndex++;
+	}
+	else if(up){
+		m_activeIndex--;
+	}
+	m_activeIndex = wrap<int>(m_activeIndex, 0, m_childMenu.size());
+	
+	MenuBase *current = m_childMenu[m_activeIndex];
+	current->action(up, down, left, right, enter, cancel);
+}
+
+void MenuRoot::draw(std::ostream &oss)
+{
+	for(int i = 0; i < m_childMenu.size(); ++i){
+		if(i == m_activeIndex){
+			oss << "> ";
+		}
+		else{
+			oss << "  ";
+		}
+		
+		m_childMenu[i]->draw(oss);
+	}
+}
+
+
+//--
+void AnimeChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	//再生するモーションを設定
+	auto &animlist = get()->getAnimationList();
+	int animNum = animlist.size();
+#if 0
+	if(left){  m_select--; }
+	if(right){ m_select++; }
+	m_select = wrap<int>(m_select, 0, animNum);
+	m_animeName = animlist[m_select];
+
+	if(enter){
+		get()->play(m_animeName);
+	}
+#endif
+}
+void AnimeChanger::draw(std::ostream &oss){
+#if 0
+	auto ssp = get();
+	oss << "animation: current " << ssp->getPlayAnimeName() << " --> " << m_animeName << endl;
+#endif
+}
+//--
+void FrameChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	SS5Player* ssp = get();
+	int animax = ssp->getMaxFrame();
+
+	if(left){  m_frame--; }
+	if(right){ m_frame++; }
+	m_frame = wrap<int>(m_frame, 0, animax);
+	
+	if(enter){
+		ssp->setCurrentFrame(m_frame);
+	}
+}
+void FrameChanger::draw(std::ostream &oss){
+	SS5Player* ssp = get();
+	oss << "frame: current=" << ssp->getCurrentFrame() << " --> " << m_frame << endl;
+}
+//--
+void PositionChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	SS5Player* ssp = get();
+	
+	if(left){  m_position--; }
+	if(right){ m_position++; }
+	if(cancel){m_position = INIT_POS;}
+
+	if(enter){
+		float x, y;
+		ssp->getPosition(&x, &y);
+
+		switch(m_xyz){
+		case XYZ::X:	x = m_position;	break;
+		case XYZ::Y:	y = m_position;	break;
+		}
+		ssp->setPosition(x, y);
+	}
+}
+void PositionChanger::draw(std::ostream &oss){
+	SS5Player* ssp = get();
+	float x, y;
+	ssp->getPosition(&x, &y);
+	switch(m_xyz){
+	case XYZ::X:
+		oss << "position X: current=" << x << " --> " << m_position << endl;
+		break;
+	case XYZ::Y:
+		oss << "position Y: current=" << y << " --> " << m_position << endl;
+		break;
+	}
+}
+//--
+void RotationChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	SS5Player* ssp = get();
+	
+	if(left){  m_rotation--; }
+	if(right){ m_rotation++; }
+	if(cancel){m_rotation = 0;}
+
+	if(enter){
+		float x,y,z;
+		ssp->getRotation(&x, &y, &z);
+
+		switch(m_xyz){
+		case XYZ::X:	x = m_rotation; break;
+		case XYZ::Y:	y = m_rotation; break;
+		case XYZ::Z:	z = m_rotation; break;
+		}
+		ssp->setRotation(x, y, z);
+	}
+}
+void RotationChanger::draw(std::ostream &oss){
+	SS5Player* ssp = get();
+	float x,y,z;
+	ssp->getRotation(&x, &y, &z);
+	
+	std::string text;
+	float current;
+	switch(m_xyz){
+	case XYZ::X:	text = "rotation X"; current=x; break;
+	case XYZ::Y:	text = "rotation Y"; current=y; break;
+	case XYZ::Z:	text = "rotation Z"; current=z; break;
+	}
+	oss << text << ": current=" << current << " --> " << m_rotation << endl;
+}
+//--
+void ScaleChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	SS5Player* ssp = get();
+	
+	if(left){  m_scale -= 0.1; }
+	if(right){ m_scale += 0.1; }
+	if(cancel){m_scale = 0.5;}
+
+	if(enter){
+		float x, y;
+		ssp->getScale(&x, &y);
+
+		switch(m_xyz){
+		case XYZ::X:	x = m_scale; break;
+		case XYZ::Y:	y = m_scale; break;
+		}
+		ssp->setScale(x, y);
+	}
+}
+void ScaleChanger::draw(std::ostream &oss){
+	SS5Player* ssp = get();
+	float x, y;
+	ssp->getScale(&x, &y);
+	
+	std::string text;
+	float current;
+	switch(m_xyz){
+	case XYZ::X:	text = "scale X"; current=x; break;
+	case XYZ::Y:	text = "scale Y"; current=y; break;
+	}
+	oss << text << ": current=" << current << " --> " << m_scale << endl;
+}
+//--
+void AlphaChanger::action(int up, int down, int left, int right, int enter, int cancel){
+	SS5Player* ssp = get();
+
+	if(left){  m_alpha -= 0.01; }
+	if(right){ m_alpha += 0.01; }
+	if(cancel){m_alpha = 1.0; }
+	m_alpha = clamp<double>(m_alpha, 0.0, 1.0);
+	
+	if(enter){
+		ssp->setAlpha(m_alpha);
+	}
+}
+void AlphaChanger::draw(std::ostream &oss){
+	SS5Player* ssp = get();
+	oss << "alpha: current=" << ssp->getAlpha() << " --> " << m_alpha << endl;
+}
+
---
+++ DxLibSample/SimpleViewer/Menu.h
@@ -0,0 +1,108 @@
+﻿#pragma once
+
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+namespace ss{
+	class SS5Player;
+}
+
+
+class MenuInterface{
+public:
+	virtual ~MenuInterface(){}
+	virtual void action(int up, int down, int left, int right, int enter, int cancel) = 0;
+	virtual void draw(std::ostream &oss) = 0;
+};
+
+
+template<class T>
+class MenuBase : public MenuInterface{
+public:
+	MenuBase(T* t) :m_context(t){}
+	virtual ~MenuBase(){}
+
+	T* get(){ return m_context; }
+	const T* get() const{ return m_context; }
+private:
+	T* m_context;
+};
+
+using Base = MenuBase<ss::SS5Player>;
+
+
+//メニューのセレクタ
+class MenuRoot : public Base{
+public:
+	MenuRoot(ss::SS5Player* p);
+	~MenuRoot();
+
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+private:
+	std::vector<Base*> m_childMenu;
+	int m_activeIndex;
+};
+
+
+//--
+struct AnimeChanger : public Base{
+	AnimeChanger(ss::SS5Player* p) : Base(p), m_select(0){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+	
+	std::string m_animeName;
+	int m_select;
+};
+
+struct FrameChanger : public Base{
+	FrameChanger(ss::SS5Player* p) : Base(p), m_frame(0){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+	int m_frame;
+};
+
+struct PositionChanger : public Base{
+	enum class XYZ{ X, Y/*, Z*/ };
+	PositionChanger(ss::SS5Player* p, XYZ xyz, int pos) : Base(p), m_xyz(xyz), m_position(pos), INIT_POS(pos){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+	XYZ m_xyz;
+	int m_position;
+	const int INIT_POS;
+};
+
+struct RotationChanger : public Base{
+	enum class XYZ{ X, Y, Z };
+	RotationChanger(ss::SS5Player* p, XYZ xyz) : Base(p), m_xyz(xyz), m_rotation(0){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+	XYZ m_xyz;
+	int m_rotation;
+};
+
+struct ScaleChanger : public Base{
+	enum class XYZ{ X, Y/*, Z*/ };
+	ScaleChanger(ss::SS5Player* p, XYZ xyz) : Base(p), m_xyz(xyz), m_scale(0.5){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+	XYZ m_xyz;
+	double m_scale;
+};
+
+struct AlphaChanger : public Base{
+	AlphaChanger(ss::SS5Player* p) : Base(p), m_alpha(1.0){}
+	void action(int up, int down, int left, int right, int enter, int cancel) override;
+	void draw(std::ostream &oss) override;
+
+	double m_alpha;
+};
+
